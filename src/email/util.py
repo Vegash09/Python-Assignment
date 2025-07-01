ca7fe0831950c418ee37afbac9d1b416c228f8e4
Vegash P <Vegash.P@diggibyte.com>
2025-07-01 18:21:07 +0530

    Made a program to check if emails are valid using regex. Took input from user, filtered the valid ones, and showed them in sorted order.

---
+++ src/email/util.py
@@ -0,0 +1,8 @@
+import re
+
+def is_valid_email(s):
+    pattern = r"^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$"
+    return bool(re.match(pattern, s))
+
+def filter_mail(emails):
+    return list(filter(is_valid_email, emails))
